88e0bfbc8532a362c27f308d8f0915798dd92e15
Yan Tapajós <ybrandao.d@gmail.com>
2024-08-04 02:56:29 +0100

    second commit

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfce7920191ce20a3f87a2aef620006475cd1d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
